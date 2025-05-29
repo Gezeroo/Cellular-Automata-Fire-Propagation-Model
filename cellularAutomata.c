bff27c3dcb89b74eb03d4469f35c681660129fde
Geraldo <contato.lri.ufu@gmail.com>
2025-05-29 09:45:56 -0300

    Implemented a proper fire spread with more states and new rules

--- cellularAutomata.c
+++ cellularAutomata.c
@@ -3,35 +3,36 @@
 #include <time.h>
 #include <stdio.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 800
-#define CELL_SIZE 10
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 1024
+#define CELL_SIZE 1
 
 #define COLS (SCREEN_WIDTH / CELL_SIZE)
 #define ROWS (SCREEN_HEIGHT / CELL_SIZE)
 
 
 typedef enum{
-    green, //vegetation 
-    orange, //initial fire
-    red, //stable fire
-    marron, //ember
-    blue,   
-    black,
-    
-    
-    gray
+    vegetation, //vegetation 
+    initial_fire, //initial fire
+    stable_fire, //stable fire
+    ember, //ember
+    water,   //water 
+    ash, //ash
+    black 
 }CellState;
 
 CellState grid[COLS][ROWS];
 CellState buffer[COLS][ROWS];
-bool type = false;
+int ticks[COLS][ROWS];
+bool type = true;
+bool paused = true;
+bool HUD = true;
 
 void InitGrid(){
     for (int x = 0; x < COLS; x++) {
         for (int y = 0; y < ROWS; y++) {
-                grid[x][y] = green;
-                buffer[x][y] = green;
+                grid[x][y] = vegetation;
+                buffer[x][y] = vegetation;
         }
     }
 }
@@ -40,11 +41,11 @@ void InitGridRandom() {
         for (int y = 0; y < ROWS; y++) {
             int r = rand() % 100;
             if(r < 5)
-                grid[x][y] = blue;
+                grid[x][y] = water;
             else if(r <= 40)
                 grid[x][y] = black;
             else
-                grid[x][y] = green;
+                grid[x][y] = vegetation;
         }
     }
 }
@@ -68,12 +69,50 @@ void contaminate(int x, int y){
     int direction[3] = {-1, 0, 1};
     int dx = rand() % 3;
     int dy = rand() % 3;
-    int nx = (x + direction[dx] + COLS) % COLS;
-    int ny = (y + direction[dy] + ROWS) % ROWS;
-    if(grid[x][y] == black)
-        if(grid[nx][ny] == green)    
-            buffer[nx][ny] = black;
-    else buffer[x][y] == grid[x][y];
+    int count = 0;
+
+    for (int dx = -1; dx <= 1; dx++) {
+        for (int dy = -1; dy <= 1; dy++) {
+            if (dx == 0 && dy == 0) continue;
+
+            int nx = (x + dx);
+            int ny = (y + dy);
+            if((nx >= COLS) || (nx <= 0) || (ny >= ROWS) || (ny <= 0)) continue;
+            if (grid[nx][ny] == initial_fire || grid[nx][ny] == stable_fire || grid[nx][ny] == ember)
+                count++;
+        }
+    }
+    if((grid[x][y] == vegetation) && (rand() % 100 < (12.5*count)))
+        buffer[x][y] = initial_fire;
+    else if(grid[x][y] == initial_fire){
+        if(ticks[x][y] > 3){
+            ticks[x][y] = 0;
+            buffer[x][y] = stable_fire;
+        }
+        else ticks[x][y]++;
+    }
+    else if(grid[x][y] == stable_fire){
+        if(ticks[x][y] > 5){
+            ticks[x][y] = 0;
+            buffer[x][y] = ember;
+        }
+        else ticks[x][y]++;
+    }
+    else if(grid[x][y] == ember){
+        if(ticks[x][y] > 10){
+            ticks[x][y] = 0;
+            buffer[x][y] = ash;
+        }
+        else ticks[x][y]++;
+    }
+    else if(grid[x][y] == ash){
+        if(rand() % 100 < (ticks[x][y])/50){
+            ticks[x][y] = 0;
+            buffer[x][y] = vegetation;
+        }
+        else ticks[x][y]++;
+    }
+    else if(grid[x][y] == water) buffer[x][y] = grid[x][y];
 }
 
 void UpdateGrid() {
@@ -83,9 +122,9 @@ void UpdateGrid() {
                 int neighbors = CountNeighbors(x, y);
                 CellState current = grid[x][y];
                 if (current == black)
-                    buffer[x][y] = (neighbors == 2 || neighbors == 3) ? black : green;
-                else if (current == green)
-                    buffer[x][y] = (neighbors == 3) ? black : green;
+                    buffer[x][y] = (neighbors == 2 || neighbors == 3) ? black : vegetation;
+                else if (current == vegetation)
+                    buffer[x][y] = (neighbors == 3) ? black : vegetation;
                 else buffer[x][y] = current;    
             }
             else
@@ -106,8 +145,12 @@ void DrawaGrid() {
         for (int y = 0; y < ROWS; y++) {
             Color color;
             switch (grid[x][y]) {
-                case green: color = GREEN; break;
-                case blue: color = BLUE; break;
+                case vegetation: color = GREEN; break;
+                case water: color = BLUE; break;
+                case initial_fire: color = ORANGE; break;
+                case stable_fire: color = RED; break;
+                case ember: color = MAROON; break;
+                case ash: color = GRAY; break; 
                 case black: color = BLACK; break;
             }
             DrawRectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, color);
@@ -117,12 +160,13 @@ void DrawaGrid() {
 
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Conway's Game of Life - Raylib");
-    SetTargetFPS(10); // Adjust speed here
-    
+    SetTargetFPS(60);
+    for(int i = 0; i < COLS; i++)
+        for(int j = 0; j < ROWS; j++)
+            ticks[i][j] = 0;
+
     InitGrid();
-    bool paused = false;
-    bool HUD = true;
-    CellState brush = green;
+    CellState brush = vegetation;
     while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_SPACE)) {
             DrawText("Paused", 10, 10, 20, RED);
@@ -130,7 +174,7 @@ int main() {
             if(paused)
                 SetTargetFPS(120);
             else
-                SetTargetFPS(10);
+                SetTargetFPS(60);
         }
         if (IsKeyPressed(KEY_R)) {
             if(type) InitGrid();
@@ -146,10 +190,11 @@ int main() {
                 buffer[x][y] = brush;
             }
         }
-        if (IsKeyPressed(KEY_G)) brush = green;
-        if (IsKeyPressed(KEY_B)) brush = blue;
+        if (IsKeyPressed(KEY_G)) brush = vegetation;
+        if (IsKeyPressed(KEY_B)) brush = water;
         if (IsKeyPressed(KEY_X)) brush = black;
         if (IsKeyPressed(KEY_E)) type = !type;
+        if (IsKeyPressed(KEY_O)) brush = initial_fire;
         if (IsKeyPressed(KEY_H)) HUD = !HUD;
 
         if (!paused) UpdateGrid();
@@ -165,13 +210,13 @@ int main() {
                 DrawText("PAUSED - Drawing Enabled", 10, 40, 20, DARKGRAY);
 
                 // Show current brush color
-                Color brushColor = (brush == green) ? GREEN : (brush == blue) ? BLUE : BLACK;
+                Color brushColor = (brush == vegetation) ? GREEN : (brush == water) ? BLUE : (brush == initial_fire) ? ORANGE : BLACK;
                 DrawText("Brush:", 10, 70, 20, DARKGRAY);
                 DrawRectangle(80, 70, 24, 24, DARKGRAY);
                 DrawRectangle(82, 72, 20, 20, brushColor);
                 
 
-                DrawText("Use G (Green), B (Blue), X (Black) to change brush", 10, 100, 20, DARKGRAY);
+                DrawText("Use G (Green), B (Blue), O (Orange), X (Black) to change brush", 10, 100, 20, DARKGRAY);
             } 
             else
                 DrawText("RUNNING - Drawing Disabled", 10, 40, 20, DARKGRAY);
